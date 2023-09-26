497d7d6e099cd28362d8822ef78a4e267f01bcfd
one-tokyo <takumi@fogca.jp>
2023-09-26 11:07:56 +0200

    0926

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+  
+<svelte:head>
+  <link rel="stylesheet" href="../css/base.css?var=1.12">
+
+</svelte:head>
+  
+  <div>
+    <slot />
+  </div>
+  
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+
+
+
+<main>
+
+    <img src="../image/KV1.webp" alt="">
+    <img src="../image/KV2.webp" alt="">
+
+</main>
+
+
+<style>
+
+img {width: 100%;}
+
+</style>
