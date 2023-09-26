9e5d3b82792512e1b8d71040dc194fe045a487f0
one-tokyo <takumi@fogca.jp>
2023-09-26 11:10:42 +0200

    0926-

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+/** @type {import('vite').UserConfig} */
+export default defineConfig(() => {
+	return {
+		server: {
+			host: true,
+			port: 3000
+		},
+		plugins: [
+			sveltekit()
+		]
+		
+	};
 });
